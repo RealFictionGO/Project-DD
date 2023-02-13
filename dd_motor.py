95570c14cdf09754acaa05d7256952facc870dc7
RealFictionGO <polandgoofficial@gmail.com>
2023-02-13 16:43:03 +0100

    project set up
    Added: project future files, website handling, and OLED handling lib

---
+++ dd_motor.py
@@ -0,0 +1,11 @@
+from machine import Pin
+
+def move_bot(go, stop, left, right):
+    if stop:
+        pass
+    elif go:
+        pass
+    elif left:
+        pass
+    elif right:
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from machine import Pin
+
+from server import *
+from face_display import *
